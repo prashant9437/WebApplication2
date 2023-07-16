0133303660685382c2a3597fa5acba35a40166c8
prashant <deshmukhprashant59@gmail.com>
2023-07-16 12:37:01 +0530

    Add project files.

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            List<Employee> lstEmps = new List<Employee>();
+
+            lstEmps.Add(new Employee { EmpNo = 1, Name = "Yash", Basic = 12345, DeptNo = 10 });
+            lstEmps.Add(new Employee { EmpNo = 2, Name = "Lalit", Basic = 12345, DeptNo = 10 });
+            lstEmps.Add(new Employee { EmpNo = 3, Name = "Saurabh", Basic = 12345, DeptNo = 10 });
+
+            return View(lstEmps);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee obj = new Employee();
+            obj.EmpNo = id;
+            obj.Name = "Vikram";
+            obj.Basic = 99999;
+            obj.DeptNo = 10;
+
+
+            return View(obj);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+           
+                return View();
+            
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                string EmpNo = collection["EmpNo"];
+                string Name = collection["Name"];
+                string Basic = collection["Basic"];
+                string DeptNo = collection["DeptNo"];
+
+               
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public int DeptNo { get; set; }
+
+
+        //Deatails of Employee
+        public static Employee GetSingleEmployee(int EmpNo)
+        {
+            Employee? emp = new Employee();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employees where EmpNo=@EmpNo";
+
+                cmd.Parameters.Add("@EmpNo", SqlDbType.Int).Value = EmpNo;
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    emp.EmpNo = dr.GetInt32("EmpNo");
+                    emp.Name = dr.GetString("Name");
+
+                }
+                else
+                {
+                    emp = null;
+                    Console.WriteLine("Employee not found!");
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return emp;
+        }
+
+        //Index: List of Employees
+
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> empLst = new List<Employee>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employees";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Employee e = new Employee();
+                    e.EmpNo = dr.GetInt32("EmpNo");
+                    e.Name = dr.GetString("EmpName");
+                    e.Basic = dr.GetDecimal("Basic");
+                    e.DeptNo = dr.GetInt32("DeptNo");
+
+                    empLst.Add(e);
+
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return empLst;
+        }
+        //Edit TABLE CONTETNT:
+        static void Edit(Employee obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=true";
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "UPDATE Employees SET EmpName = @EmpName, Basic = @Basic, DeptNo = @DeptNo,  WHERE EmpNo = @EmpNo";
+
+                cmd.Parameters.AddWithValue("@EmpNo", obj.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", obj.EmpName);
+                cmd.Parameters.AddWithValue("@Basic", obj.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Employee Details Updated Successfully");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        //Delete:
+        static void Delete(int EmpNo)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                try
+                {
+                    cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=true";
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "DELETE FROM Employees WHERE EmpNo = @EmpNo";
+
+                    cmd.Parameters.AddWithValue("@EmpNo", EmpNo);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    Console.WriteLine("Employee Deleted Successfully");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+
+    }
+}
